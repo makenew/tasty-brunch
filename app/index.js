408ede364bf098e3476791ab18facf66ce02e9b2
Evan Sosenko <razorx@evansosenko.com>
2016-02-11 17:14:41 -0800

    Render app in index.js

--- app/index.js
+++ app/index.js
@@ -1,2 +1,25 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+
+import counter from './reducers/counter'
+import Counter from './components/Counter'
+
+const store = createStore(counter)
+const rootEl = document.getElementById('root')
+
+function render () {
+  ReactDOM.render(
+    <Counter
+      value={store.getState()}
+      onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
+      onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
+    />,
+    rootEl
+  )
+}
+
 export default () => {
+  render()
+  store.subscribe(render)
 }
