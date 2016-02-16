604d1048a8751e68e0a077ec49941b28aa9daf3a
Evan Sosenko <razorx@evansosenko.com>
2016-02-15 20:09:58 -0800

    Use gulp-load-plugins

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,7 @@ const fs = require('fs')
 
 const gitRevSync = require('git-rev-sync')
 const gulp = require('gulp')
-const ghPages = require('gulp-gh-pages')
-const htmlhint = require('gulp-htmlhint')
-const htmlmin = require('gulp-htmlmin')
-const plumber = require('gulp-plumber')
-const sassLint = require('gulp-sass-lint')
-const standard = require('gulp-standard')
-const watch = require('gulp-watch')
+const $ = require('gulp-load-plugins')()
 
 const pkg = require('./package.json')
 
@@ -17,48 +11,48 @@ gulp.task('lint', ['standard', 'sass-lint'])
 
 gulp.task('htmlhint', () => {
   return gulp.src('public/**/*.html')
-    .pipe(htmlhint())
-    .pipe(htmlhint.failReporter())
+    .pipe($.htmlhint())
+    .pipe($.htmlhint.failReporter())
 })
 
 gulp.task('standard', () => {
   return gulp.src('app/**/*.js')
-    .pipe(standard())
-    .pipe(standard.reporter('default', {
+    .pipe($.standard())
+    .pipe($.standard.reporter('default', {
       breakOnError: true
     }))
 })
 
 gulp.task('sass-lint', () => {
   return gulp.src('app/**/*.scss')
-    .pipe(sassLint())
-    .pipe(sassLint.format())
-    .pipe(sassLint.failOnError())
+    .pipe($.sassLint())
+    .pipe($.sassLint.format())
+    .pipe($.sassLint.failOnError())
 })
 
 gulp.task('watch', () => {
   gulp.src('public/**/*.html')
-    .pipe(watch('public/**/*.html'))
-    .pipe(plumber())
-    .pipe(htmlhint())
-    .pipe(htmlhint.reporter())
+    .pipe($.watch('public/**/*.html'))
+    .pipe($.plumber())
+    .pipe($.htmlhint())
+    .pipe($.htmlhint.reporter())
 
   gulp.src('app/**/*.js')
-    .pipe(watch('app/**/*.js'))
-    .pipe(plumber())
-    .pipe(standard())
-    .pipe(standard.reporter('default'))
+    .pipe($.watch('app/**/*.js'))
+    .pipe($.plumber())
+    .pipe($.standard())
+    .pipe($.standard.reporter('default'))
 
   return gulp.src('app/*/**/.scss')
-    .pipe(watch('app/**/*.scss'))
-    .pipe(plumber())
-    .pipe(sassLint())
-    .pipe(sassLint.format())
+    .pipe($.watch('app/**/*.scss'))
+    .pipe($.plumber())
+    .pipe($.sassLint())
+    .pipe($.sassLint.format())
 })
 
 gulp.task('minify', () => {
   return gulp.src('public/**/*.html')
-    .pipe(htmlmin({
+    .pipe($.htmlmin({
       collapseWhitespace: true,
       preserveLineBreaks: true,
       minifyJS: true
@@ -70,7 +64,7 @@ gulp.task('deploy', () => {
   fs.openSync('public/.nojekyll', 'w')
 
   return gulp.src(['public/.nojekyll', 'public/**/*'])
-    .pipe(ghPages({
+    .pipe($.ghPages({
       remoteUrl: `git@github.com:${pkg.repository}.git`,
       message: `Deploy ${gitRevSync.short()} from v${pkg.version}`
     }))
